858a7f0857f0ea0727dd650b1c52d36cc85b2866
wuanghn <wuanghn@gmail.com>
2015-04-09 17:14:36 +0700

    update last day

--- app/views/layouts/frontend/header.blade.php
+++ app/views/layouts/frontend/header.blade.php
@@ -4,12 +4,12 @@
  										
  										<ul class="nav nav-pills">
  										  <li role="presentation">
- 										  		<a class="wa_fix_bkg_logo" style="padding-top:0px" href="#">
+ 										  		<a class="wa_fix_bkg_logo" style="padding-top:0px" href="{{url('/')}}">
  										  				<img src="{{url('public/frontend/img/logo.png')}}" height="53" width="187">
  										  		</a>
  										  </li>
- 										  <li role="presentation" class="active"><a href="#">Search</a></li>
- 										  <li role="presentation"><a href="http://vnw2.slifemedia.com/blog">Blog</a></li>
+ 										  <li role="presentation" class="active"><a href="{{url('/')}}">Search</a></li>
+ 										  <li role="presentation"><a href="{{ url('blog') }}">Blog</a></li>
  										</ul>
  								</div>
 
